681420d02b060915ec48114147f08dd5b7c5df34
Alex Lehtonen <alexlehtonen@live.fi>
2024-04-16 11:15:31 +0300

    folders

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+	<!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+
+
+
+
+
+ 
+
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2024 Copyright:
+    <a class="text-reset fw-bold" href="">Programutvecklarna 2022</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+
+</body>
+</html>
